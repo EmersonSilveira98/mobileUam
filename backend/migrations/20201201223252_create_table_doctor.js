5cde6383dd4c540ae34ca21892eaf58b50b34714
Gustavo <saopaulo.gustavoo@hotmail.com>
2020-12-01 21:35:16 -0300

    Tabela doctor

--- backend/migrations/20201201223252_create_table_doctor.js
+++ backend/migrations/20201201223252_create_table_doctor.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+    return knex.schema.createTable('doctor', table =>{
+      table.increments('id').primary()
+      table.string('name')
+      table.string('date')
+    })
+  };
+   
+  exports.down = function(knex) {
+    return knex.schema.dropTable('doctor')
+  };
