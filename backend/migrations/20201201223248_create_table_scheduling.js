f4f3d7cd784200101134f67ad8dcd1f76d8fe458
Gustavo <saopaulo.gustavoo@hotmail.com>
2020-12-01 21:37:29 -0300

    tabela de agendamento

--- backend/migrations/20201201223248_create_table_scheduling.js
+++ backend/migrations/20201201223248_create_table_scheduling.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex) {
-  
-};
-
+    return knex.schema.createTable('scheduling', table => {
+        table.increments('id').primary()
+        table.integer('doctorId').references('id')
+            .inTable('doctor')
+        table.string('date').notNull()
+        table.string('schedule').notNull()
+        table.integer('userId').references('id')
+          .inTable('users')
+    })
+        };
+ 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('scheduling')
 };
