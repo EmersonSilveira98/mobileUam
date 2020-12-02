7594bebf8ed28cb6434ec9a3b4fa711bd282fe31
lmggranja <lmgomes016@gmail.com>
2020-12-01 21:55:40 -0300

    script user

--- app/src/app/login/login.page.ts
+++ app/src/app/login/login.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {​​​​​ Component, OnInit }​​​​​ from '@angular/core';
+import {​​​​​ Router, RouterLink }​​​​​ from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-login',
@@ -6,13 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-public email;
-public password;
+  public user= {​​​​​}​​​​​;
+  public url = 'http://localhost:3333/signin'
 
-signin(){
-  console.log(`Email: ${this.email}, Senha: ${this.password}`)
-}
-  constructor() { }
+signin(){​​​​​​​
+  axios.post(this.url, this.user)
+    .then(res => {​​​​​​​
+      axios.defaults.headers.common['Authorization'] = `bearer ${​​​​​​​res.data.token}​​​​​​​`;
+      localStorage.setItem('userId', JSON.stringify(res.data.id));
+      this.router.navigateByUrl('/home');
+    }​​​​​​​)
+}​​​​​​​
+
+constructor(private router: Router) {​​​​​ }​​​​​
 
   ngOnInit() {
     
